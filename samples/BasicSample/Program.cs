e2a4d25dc03517e54d35af322573c864c782fd86
Michael Monsour <michael@lethek.net>
2022-07-08 22:17:25 +1000

    Fixed FFmpeg.AutoGen reference. Added a basic sample console app.

---
+++ samples/BasicSample/Program.cs
@@ -0,0 +1,9 @@
+﻿using FFmpeg.Loader;
+
+try {
+    string version = FFmpegLoader.RegisterBinaries();
+    Console.WriteLine($"Loaded FFmpeg v{version}");
+
+} catch (DllNotFoundException) {
+    Console.WriteLine("Could not find FFmpeg");
+}
